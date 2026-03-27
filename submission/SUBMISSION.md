8787f5b46010e58bcd216f533c6d21f96a8b8051
Krishh Sood <KrishhSood@student.unsw.edu.au>
2026-03-27 20:01:39 +1100

    Finalised the product - ready for submission

--- submission/SUBMISSION.md
+++ submission/SUBMISSION.md
@@ -4,5 +4,25 @@
 https://www.tinkercad.com/things/hicKVTlOgNJ-terrific-fyyran-borwo/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard&sharecode=bJj9ES8dsHtZhK-3XMb5opYSAW9_lS8HnqSe8hWGNjU
 ```
 
+## Changes
+I added the pulse, so now the LED fades.
+
+I added the feedback led flashing, according to the spec.
+
+I added comments and documentation.
+
+Improved the overall UX for example
+ - I r&d'd feedback timings to see what is most understandable
+ - I capped the brightness at 68, so the led pulses between 0 and 68, so that when it turns on at full brightness it is distinct. It should ideally be even more distinct, however, lowering the brightness is one of the solutions I could come up with while remaining within the spec
+
+
 ## Program Explanation
-The delay() would stop the arduino from sensing inputs, then we can't detect false starts. Fixed that. Added false start functionality correctly.
+The program initializes the LED and button pins, sets up serial tracking, and uses an unconnected analog pin to read environmental electrical noise. This creates a truly random seed, ensuring the game's wait times are unpredictable every time it boots. When the user presses the button to start, a brief delay provides a grace period to prevent accidental double-clicks before calculating a random wait time between two and ten seconds.
+
+During the armed phase, the program uses a non-blocking timer instead of a standard delay. This allows the system to actively listen for early button presses while simultaneously fading the LED up and down to indicate it is waiting. The maximum brightness of this pulse is mathematically capped at 68 out of 255. This ensures the dim waiting pulse is distinctly different from the full-brightness signal. If a user clicks early, the system registers a false start, flashes a penalty warning, and resets.
+
+If the user waits successfully, the LED snaps to maximum brightness as the starting signal. The program records the exact millisecond and traps the system in a loop until the button is pressed, allowing it to accurately calculate the reaction speed. Finally, it logs the attempt count and running average to the serial monitor, and provides immediate gamified feedback by flashing the LED at varying speeds based on the user's performance before resetting for the next round. 
+
+This was an extremely refreshing and enjoyeable simple project after a long time. 
+
+As per the spec the fast and medium responses are below 350ms, which in my experience is too quick given the lags on our computers and tinkercad browser (Or I am just slow :-(   ), given the freedom, I would increase those times. As well as that, there is an unending potential of functionality that could be added here, an LCD display, audio feedback, gameified high score memory, data & analytics and so much more.
--- submission/code.cpp
+++ submission/code.cpp
@@ -1,4 +1,17 @@
-// Put this code in tinkercad with the circuit to see
+/*
+ * PROJECT: Arduino Reaction Time Tester
+ * AUTHOR: Krishh Sood
+ * Date: 2024-06-20
+ * For Bluesat Software Applicant Technical Assessment
+ * 
+ * DESCRIPTION: A game that tests human reaction speed. The system pulses 
+ * an LED at low brightness while waiting a random amount of time. If the user 
+ * presses early, it logs a False Start. Once the LED goes 
+ * solid, the user must press the button as fast as possible. 
+ * The system calculates the reaction time, averages it over 
+ * multiple attempts, and provides visual LED feedback.
+ * 
+ * */
 
 // constants
 const int ledPin = 11;
@@ -9,49 +22,93 @@ int numAttempts = 0;
 long totalTime = 0;
 int numFalse = 0;
 
+int brightness = 0;             // 0 brightness to begin
+int fadeAmount = 4;             // How many steps it takes at a time //fix
+unsigned long lastFadeTime = 0; // A stopwatch just for the fading //fix
+
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   
   Serial.begin(9600);
-  randomSeed(analogRead(A0)); // truely random seed
+  randomSeed(analogRead(A0));   // truely random seed
   Serial.println("Click the button to start a round!");
 }
 
 void loop() {
+  // IDLE
+  // LED is off
+  // The system waits for a button press to arm the device.
   if (digitalRead(buttonPin) == HIGH) {
-    delay(300); // prevent double press
+    
+    // a delay to prevent accidental double presses
+    delay(300); 
 
     // random wait time
     int waitTime = random(2000, 10000);
     long startWait = millis();
     bool falseStart = false;
 
-    // wait using millis() not delay() to allow detection for false starts
+    // ARMED
+    // LED slowly pulses
+    // The device waits a random duration between 2 and 10 seconds
+    // If the button is pressed during this phase, record a false start
     while (millis() - startWait < waitTime) {
+      
+      // led pulsing
+      // check every 50 milliseconds for a slow fade
+      if (millis() - lastFadeTime > 50) {
+
+        brightness = brightness + fadeAmount;   // Take a step (adds 5)
+
+        // the brightness pulses from 68 to 0 with increments of 4
+        // 68 and not 255, since 255 brightness should be distinct for when the LED is fully on
+        if (brightness <= 0 || brightness >= 68) {
+          fadeAmount = -fadeAmount;             // flip the sign and traverse backwards
+        }
+
+        analogWrite(ledPin, brightness);
+        lastFadeTime = millis();
+      }
+
+      // false start
       if (digitalRead(buttonPin) == HIGH) {
         falseStart = true;
         break;
       }
     }
 
+    // false start check
     if (falseStart == true) {
       numFalse++;
       Serial.println("False Start! You pressed the button too early.");
       Serial.print("Total False Starts: ");
       Serial.println(numFalse);
-      delay(2000); // Penalty delay
+      
+      // quick flash for 2 seconds
+      for (int i = 0; i < 10; i++) {
+        digitalWrite(ledPin, HIGH);
+        delay(100);
+        digitalWrite(ledPin, LOW);
+        delay(100);
+      }
       return;
     }
 
     // Reaction
+    // LED turns on instantly as the signal
+    // Start the timer and record the reaction time when the button is pressed
     digitalWrite(ledPin, HIGH);
     long startTiming = millis();
 
-    while (digitalRead(buttonPin) == LOW) { }
+    while (digitalRead(buttonPin) == LOW) {
+      // wait
+    }
 
+    // reaction time calculation
     long reactionTime = millis() - startTiming;
     numAttempts++;
+
     totalTime = totalTime + reactionTime;
     long averageTime = totalTime / numAttempts;
 
@@ -61,7 +118,23 @@ void loop() {
     Serial.print("False Starts: "); Serial.println(numFalse);
     Serial.println("--------------------");
 
-    digitalWrite(ledPin, LOW); 
+    digitalWrite(ledPin, LOW);  // turn off
+
+    // OUTPUT AND FEEDBACK
+    if (reactionTime < 200) {
+      // fast blink
+      for (int i = 0; i < 8; i++) { digitalWrite(ledPin, HIGH); delay(80); digitalWrite(ledPin, LOW); delay(80); }
+    } 
+    else if (reactionTime < 350) {
+      // medium blink
+      for (int i = 0; i < 5; i++) { digitalWrite(ledPin, HIGH); delay(250); digitalWrite(ledPin, LOW); delay(250); }
+    } 
+    else {
+      // slow blink
+      for (int i = 0; i < 2; i++) { digitalWrite(ledPin, HIGH); delay(400); digitalWrite(ledPin, LOW); delay(400); }
+    }
+
+    // wait before next round
     delay(1000); 
   }
 }
